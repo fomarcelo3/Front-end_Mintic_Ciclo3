684d6baf65c6485002a7279203f0a4619f8dff72
Fredy Marcelo <fredy.marcelo.mt@correo.usa.edu.co>
2021-10-20 23:22:42 -0500

    Se agregan vistas para categorias y reservas

---
+++ js/categoria/funciones.js
@@ -0,0 +1,71 @@
+function validaesVacio(dato){
+    return !dato.trim().length;
+}
+
+/**
+ * Al ingresar un nuevo registro:
+ * 
+ * Ejecuta validaciones campo a campo
+ */
+function validar(){
+    //obtiene valores
+    let name = $("#name").val();
+    let description= $("#description").val();
+    let errores="";
+    $("#mensajes").html("");
+
+    //valida que los campos no sean vacios
+    if( validaesVacio(name)) {
+        errores="name vacio<br>";
+        $("#mensajes").html(errores);
+        $("#mensajes").show(500);
+        $("#name").focus();
+        return false;
+    }else if( validaesVacio(description)) {
+        errores="description vacio<br>";
+        $("#mensajes").html(errores);
+        $("#mensajes").show(500);
+        $("#description").focus();
+        return false;
+    }else{
+        $("#mensajes").html("");
+        $("#mensajes").hide(500);
+        return true;
+    }
+
+    return true;
+}
+
+/**
+ * Al actualizar un nuevo registro:
+ * 
+ * Ejecuta validaciones campo a campo
+ */
+ function validarEditar(){
+    //obtiene valores
+    let id = $("#idEdit").val();
+    let mensajeEdit = $("#mensajeEdit").val();
+    let errores="";
+    $("#mensajes").html("");
+
+    //valida que los campos no sean vacios
+    if( validaesVacio(id)) {
+        errores="id vacio<br>";
+        $("#mensajes").html(errores);
+        $("#mensajes").show(500);
+        $("#idEdit").focus();
+        return false;
+    }else if( validaesVacio(mensajeEdit)) {
+        errores="Mensaje vacio<br>";
+        $("#mensajes").html(errores);
+        $("#mensajes").show(500);
+        $("#nameEdit").focus();
+        return false;
+    }else{
+        $("#mensajes").html("");
+        $("#mensajes").hide(500);
+        return true;
+    }
+
+    return true;
+}
---
+++ js/reserva/funciones.js
@@ -0,0 +1,78 @@
+function validaesVacio(dato){
+    return !dato.trim().length;
+}
+
+/**
+ * Al ingresar un nuevo registro:
+ * 
+ * Ejecuta validaciones campo a campo
+ */
+function validar(){
+    //obtiene valores
+    let mensaje = $("#mensaje").val();
+    let idClient= $("#client_id").val()
+    let id = $("#costume_id").val();
+    let errores="";
+    $("#mensajes").html("");
+
+    //valida que los campos no sean vacios
+    if( validaesVacio(id)) {
+        errores="id vacio<br>";
+        $("#mensajes").html(errores);
+        $("#mensajes").show(500);
+        $("#id").focus();
+        return false;
+    }else if( validaesVacio(mensaje)) {
+        errores="mensaje vacio<br>";
+        $("#mensajes").html(errores);
+        $("#mensajes").show(500);
+        $("#name").focus();
+        return false;
+    }else if( validaesVacio(idClient)) {
+        errores="idClient vacio<br>";
+        $("#mensajes").html(errores);
+        $("#mensajes").show(500);
+        $("#idClient").focus();
+        return false;
+    }else{
+        $("#mensajes").html("");
+        $("#mensajes").hide(500);
+        return true;
+    }
+
+    return true;
+}
+
+/**
+ * Al actualizar un nuevo registro:
+ * 
+ * Ejecuta validaciones campo a campo
+ */
+ function validarEditar(){
+    //obtiene valores
+    let id = $("#idEdit").val();
+    let mensajeEdit = $("#mensajeEdit").val();
+    let errores="";
+    $("#mensajes").html("");
+
+    //valida que los campos no sean vacios
+    if( validaesVacio(id)) {
+        errores="id vacio<br>";
+        $("#mensajes").html(errores);
+        $("#mensajes").show(500);
+        $("#idEdit").focus();
+        return false;
+    }else if( validaesVacio(mensajeEdit)) {
+        errores="Mensaje vacio<br>";
+        $("#mensajes").html(errores);
+        $("#mensajes").show(500);
+        $("#nameEdit").focus();
+        return false;
+    }else{
+        $("#mensajes").html("");
+        $("#mensajes").hide(500);
+        return true;
+    }
+
+    return true;
+}
